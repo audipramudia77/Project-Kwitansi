0378a2870bc35d25bb7f5cb31d55c46017e52a6c
audipramudia77 <58422190+audipramudia77@users.noreply.github.com>
2022-08-12 16:06:54 +0800

    Magang

---
+++ database/migrations/2022_08_12_061242_create_pejabats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pejabats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('opd_id');
+            $table->string('nama_lengkap');
+            $table->integer('nip');
+            $table->string('jabatan');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pejabats');
+    }
+};
